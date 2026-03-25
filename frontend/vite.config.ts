5e5893dabe51c4229f9e083887d6d785a401fa32
NicoloLattanzio <nicololattanzio@gmail.com>
2026-03-25 16:52:47 +0100

    commit setup iniziale con docker

---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3001',   // nome del servizio in docker-compose
+        changeOrigin: true,
+      },
+    },
+  },
+})
+
+export default config
