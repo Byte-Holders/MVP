f42cca09e76d4bae7ccfe6cba99a98c1fe11b0a5
Damiano1234 <damianoberti04@gmail.com>
2026-03-27 17:45:30 +0100

    aggiunta connessione a mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get<string>('DB_USER')}:${configService.get<string>('DB_PASSWORD')}@${configService.get<string>('DB_IP')}:${configService.get<string>('DB_PORT')}/${configService.get<string>('DB_NAME')}?authSource=admin`,
+      }),
+    }),
+    WorkspaceModule 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
