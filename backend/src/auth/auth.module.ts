80d819c735ce9e4d0f154a6a619e7c1619e8ac86
Damiano1234 <damianoberti04@gmail.com>
2026-04-01 23:16:24 +0200

    fix auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { UserModule } from 'src/user/user.module';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
+@Global()
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' }), UserModule],
-  providers: [JwtStrategy],
+  providers: [JwtStrategy, JwtAuthGuard],
+  exports: [PassportModule, JwtAuthGuard]
 })
 export class AuthModule {}
