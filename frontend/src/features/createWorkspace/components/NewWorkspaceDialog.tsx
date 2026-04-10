73fbbbbceda6c082e7122dd3e0ee08fb8569b12e
Giulia2424 <giuliarom4@gmail.com>
2026-04-10 13:14:18 +0200

    sistemati degli errori in createWorkspace

--- frontend/src/features/createWorkspace/components/NewWorkspaceDialog.tsx
+++ frontend/src/features/createWorkspace/components/NewWorkspaceDialog.tsx
@@ -43,7 +43,7 @@ export function NewWorkspaceDialog() {
           <FieldGroup>
             <form.Field
               name="name"
-              children={(field) => {
+              children={(field: any) => {
                 const isInvalid =
                   field.state.meta.isTouched && !field.state.meta.isValid
                 return (
