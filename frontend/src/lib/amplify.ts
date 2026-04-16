4de148859646f5b892524f81812932052a7ef3a8
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2026-04-16 16:11:47 +0200

    Amplify

--- frontend/src/lib/amplify.ts
+++ frontend/src/lib/amplify.ts
@@ -13,7 +13,7 @@ Amplify.configure({
       loginWith: {
         oauth: {
           domain: cognitoConfig.domain,
-          scopes: ['openid', 'email', 'phone', 'aws.cognito.signin.user.admin'],
+          scopes: ['openid', 'email', 'profile'],
           redirectSignIn: [
             'https://develop.dfk7n70x1c1hn.amplifyapp.com/callback',
             'http://localhost:3000/callback',
