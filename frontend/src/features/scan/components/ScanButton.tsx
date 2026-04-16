b4613a721deec479838e898f6529e4f27072f687
Lorenzo <lore.grolla04@gmail.com>
2026-04-17 01:04:34 +0200

    fix bug

--- frontend/src/features/scan/components/ScanButton.tsx
+++ frontend/src/features/scan/components/ScanButton.tsx
@@ -50,15 +50,7 @@ export function ScanButton({
     }
     queryClient.removeQueries({ queryKey: ['scanStatus', scanId] })
     reset()
-  }, [
-    scanStatus,
-    onCompleted,
-    reset,
-    queryClient,
-    scanId,
-    repositoryId,
-    branch,
-  ])
+  }
 
   useEffect(() => {
     onScanActiveChange?.(scanRunning)
