e76b5ca70c691dec443218c13ca4870d540ba39c
Giulia2424 <giuliarom4@gmail.com>
2026-04-17 12:08:11 +0200

    swagger api Workspace Manager

--- backend/src/workspace/workspaceManager/dtos/CreateWorkspaceResponseDto.ts
+++ backend/src/workspace/workspaceManager/dtos/CreateWorkspaceResponseDto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger/dist/decorators/api-property.decorator';
 import { IsString } from 'class-validator';
 
 export class CreateWorkspaceResponseDto {
+  @ApiProperty({
+    description: 'ID del workspace',
+  })
   @IsString()
   id!: string;
 
+  @ApiProperty({
+    description: 'Nome del workspace',
+  })
   @IsString()
   name!: string;
 
+  @ApiProperty({
+    description: 'Username del creatore del workspace',
+  })
   @IsString()
   ownerUsername!: string;
 }
