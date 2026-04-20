3eb4699906bda93d64d240849e4d9e977b71713d
Damiano1234 <damianoberti04@gmail.com>
2026-04-20 16:36:18 +0200

    aggiunto controllo project manager a removeUserFromworkspace

--- backend/src/workspace/workspaceUser/interfaces/IWorkspaceUserService.ts
+++ backend/src/workspace/workspaceUser/interfaces/IWorkspaceUserService.ts
@@ -2,7 +2,7 @@ import { WorkspaceRole } from 'src/workspace/roles.enum';
 import { UserOfWorkspaceInfo } from '../type/userOfWorkspace.type';
 
 export interface IWorkspaceUserService {
-  removeUserFromWorkspace(workspaceId: string, userId: string): Promise<void>;
+  removeUserFromWorkspace(workspaceId: string, userId: string, removerOfUserId: string): Promise<void>;
   getUsersOfWorkspace(workspaceId: string): Promise<UserOfWorkspaceInfo[]>;
   getUserRoleForRepository(
     repositoryId: string,
